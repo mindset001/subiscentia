125ab20753f821da74723ad0ee3fc53e283d467b
mindset001 <shittumoshoodademola@gmail.com>
2025-10-28 16:45:00 +0100

    'neat'

--- src/app/collections/[id]/details/page.tsx
+++ src/app/collections/[id]/details/page.tsx
@@ -1,9 +1,13 @@
 import CollectionDetails from '@/components/collections/[id]/details/CollectionDetails';
 import { notFound } from 'next/navigation';
 
-// Remove incorrect import. Define local PageProps type below.
+type DetailsPageProps = {
+  params: {
+    id: string;
+  };
+};
 
-export default function DetailsPage({ params }: { params: { id: string } }) {
-  if (!params.id) return notFound();
+export default function DetailsPage({ params }: DetailsPageProps) {
+  if (!params?.id) return notFound();
   return <CollectionDetails collectionId={params.id} />;
 }
